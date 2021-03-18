c62b821fa9840d6b0fa387c3af8f7a6d12f68945
Flavio R. Troszczanczuk <flavioczuk@gmail.com>
2021-03-18 16:10:54 +0000

    temp

---
+++ temp/assets/js/functions.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+	
+	// WOW
+	wow = new WOW({
+		live: true
+	}).init();
+
+	// REPLACE ALL SVG IMAGES WITH INLINE SVG
+	jQuery('img.svg').each(function(){
+		var $img = jQuery(this);
+		var imgID = $img.attr('id');
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+		jQuery.get(imgURL, function(data) {
+			var $svg = jQuery(data).find('svg');
+			if(typeof imgID !== 'undefined') {
+				$svg = $svg.attr('id', imgID);
+			}
+			if(typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass+' replaced-svg');
+			}
+			$svg = $svg.removeAttr('xmlns:a');
+			$img.replaceWith($svg);
+		}, 'xml');
+	});
+
+	// RENDER AGAIN SVGS THAT WERE INSERTED AFTER THE PAGE LOADED
+	setTimeout(function(){
+		jQuery('img.svg').each(function(){
+			var $img = jQuery(this);
+			var imgID = $img.attr('id');
+			var imgClass = $img.attr('class');
+			var imgURL = $img.attr('src');
+			jQuery.get(imgURL, function(data) {
+				var $svg = jQuery(data).find('svg');
+				if(typeof imgID !== 'undefined') {
+					$svg = $svg.attr('id', imgID);
+				}
+				if(typeof imgClass !== 'undefined') {
+					$svg = $svg.attr('class', imgClass+' replaced-svg');
+				}
+				$svg = $svg.removeAttr('xmlns:a');
+				$img.replaceWith($svg);
+			}, 'xml');
+		});
+	}, 200);
+
+});
